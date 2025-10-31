86610092dde9af4fa47aa6b8e4071efc0a1f0b0e
kimty15 <57588446+kimty15@users.noreply.github.com>
2025-10-31 04:24:52 +0000

    Done testing

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from testing!")
+from spire.xls import *
+from spire.xls.common import *
+def excel2image(path:str, save_path:str):
+    workbook = Workbook()
+    workbook.LoadFromFile(path)
+    worksheet = workbook.Worksheets[0]
+    # worksheet.to_image().save(path)
+    image = worksheet.ToImage(worksheet.FirstRow, worksheet.FirstColumn, worksheet.LastRow, worksheet.LastColumn)
+    image.Save(save_path)
+    workbook.Dispose()
 
+path = r"/workspaces/testing/06J0690_SI_VGM_SP_2EF56.xls"
+save_path = "demo.png"
 
-if __name__ == "__main__":
-    main()
+excel2image(path, save_path)
